32fbcccea1b189a8f06654a19794aeb10181ecd1
Sultan Mukhtarov <suli419956@gmail.com>
2024-06-17 07:34:35 +0600

    created an async function of sendingmessage. same url. same await fetch and same msgtype with prevstates. and a bit of bootstraps.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Message from './components/messages/Message';
 const App = () => {
     return (
         <div>
-            < Message />
+            <Message />
         </div>
     );
 };
--- src/components/messages/Message.tsx
+++ src/components/messages/Message.tsx
@@ -3,14 +3,12 @@ import {Message as MessageType} from '../../types';
 
 const Message: React.FC = () => {
   const [messages, setMessages] = useState<MessageType[]>([]);
-  const [lastMessage, setLastMessage] = useState('');
+  const [lastMessage, setLastMessage] = useState<string>('');
+  const [newMessage, setNewMessage] = useState<string>('');
+  const [author, setAuthor] = useState<string>('')
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      void fetchMessages();
-    }, 3000);
-    return () => clearInterval(interval);
-  }, [lastMessage]);
+
+
 
   const fetchMessages = async () => {
     try {
@@ -26,16 +24,61 @@ const Message: React.FC = () => {
     }
   };
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      void fetchMessages();
+    }, 2000);
+    return () => clearInterval(interval);
+  }, [fetchMessages,lastMessage]);
+
+  const sendMessage = async () => {
+    const url = 'http://146.185.154.90:8000/messages';
+
+    const data = new URLSearchParams();
+    data.set('message', newMessage);
+    data.set('author', author);
+
+    try {
+      const response = await fetch(url, {
+        method: 'post',
+        body: data,
+      });
+
+      const sendMessage: MessageType = await response.json()
+      setMessages((prevState) => [...prevState, sendMessage]);
+      setNewMessage('');
+    } catch (err) {
+      console.error('Err')
+    }
+  }
+
 
 
   return (
-    <div>
-      {messages.map((message, index) => (
-        <div key={index}>
-          <p>{message.author}: {message.text}</p>
-          <p>{message.datetime}</p>
-        </div>
-      ))}
+    <div className="container mt-4">
+      <h1 className="mb-4">Messages</h1>
+      <div className="list-group">
+        {messages.map((message, index) => (
+          <div key={index} className="list-group-item">
+            <div className="d-flex justify-content-between">
+              <h5 className="mb-2">{message.author}</h5>
+              <small>{new Date(message.datetime).toLocaleString()}</small>
+            </div>
+            <p className="mb-1">{message.text}</p>
+          </div>
+        ))}
+      </div>
+      <input placeholder="User Name"
+             className="form-control mt-5 mb-2"
+             type="text" value={author}
+             onChange={(e) => setAuthor(e.target.value)}/>
+      <input placeholder="Message here"
+             className="form-control mt-3 mb-2"
+             type="text" value={newMessage}
+             onChange={(e) => setNewMessage(e.target.value)}/>
+
+
+      <button className="btn btn-primary" onClick={sendMessage}>Send</button>
     </div>
   );
 };
